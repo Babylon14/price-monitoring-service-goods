2aec173a80a68b64311027853f2ac5c2b7fa15c0
Andrey Zablotskiy <zablotskyandrew@yandex.ru>
2026-02-09 10:00:03 +0300

    Добавлен репозиторий для работа с БД модели Продукт, и методы для создания продукта и получения его по url.

---
+++ app/repositories/product_repository.py
@@ -0,0 +1,29 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+
+from app.models.product import Product
+
+
+class ProductRepository:
+    def __init__(self, db: AsyncSession):
+        self.db = db
+
+
+    async def get_by_url(self, url: str) -> Product | None:
+        """Метод для получения продукта по url"""
+        result = await self.db.execute(select(Product).where(Product.url == url))
+        return result.scalar_one_or_none()
+    
+    
+    async def create(self, **kwargs) -> Product:
+        """Метод для создания продукта"""
+        instance = Product(**kwargs)
+        self.db.add(instance)
+        await self.db.commit()
+        await self.db.refresh(instance)
+        return instance
+
+
+    
+
+
